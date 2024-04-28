5704b5a05604abb7d0e19082cd62fa2f5e069392
Cong Zhang <dancerphil1994@gmail.com>
2024-04-28 17:10:25 +0800

    0.0.12 Add deprecated information

--- src/Field/FieldLayout.tsx
+++ src/Field/FieldLayout.tsx
@@ -22,6 +22,10 @@ const getClassName = ({hasGap, width}: Params) => css`
 export interface FieldLayoutProps extends Omit<FormItemProps, 'help' | 'rules' | 'shouldUpdate' | 'dependencies' | 'validateDebounce' | 'validateTrigger' | 'validateFirst'> {
     width?: number;
     hasGap?: boolean;
+    /** @deprecated help 被用于显示 error 信息，且会影响间距，建议使用 extra 代替。 */
+    help?: FormItemProps['help'];
+    /** @deprecated rules 作为 antd 的字段无法与表单联动，建议使用 validate 代替。 */
+    rules?: FormItemProps['rules'];
     // 开源版本没有以下的
     extraChildren?: ReactNode;
 }
